646c9a3805f97214eadf39aa514cfd9adf62626b
ani4ka656 <annavasileva_@abv.bg>
2018-07-08 21:38:22 +0300

    almost done

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 var clicks = 0,
-    submit=document.getElementById('submit'),
+    submit = document.getElementById('submit'),
     btn = document.getElementById('btn'),
     text = document.getElementById('text'),
     rules = document.getElementById('rules'),
     message = document.getElementById('message'),
     guesses =  document.getElementById('guesses'),
     number =  document.getElementById('number')
-    rules.style.display= "none";
+    rules.style.display = "none";
+
 btn.addEventListener('mouseenter', function(){             
-    rules.style.display= "block";
-    message.style.display= "none";
-    guesses.style.display= "none";
-    number.style.display= "none";
-    text.style.display= "none";
-    submit.style.display= "none";
+    rules.style.display = "block";
+    message.style.display = "none";
+    guesses.style.display = "none";
+    number.style.display = "none";
+    text.style.display = "none";
+    submit.style.display = "none";
 });
+
 btn.addEventListener('mouseout', function(){ 
-    rules.style.display= "none";
-    message.style.display= "block";
-    guesses.style.display= "block";
-    number.style.display= "block";
-    text.style.display= "block";
-    submit.style.display= "block";
+    rules.style.display = "none";
+    message.style.display = "block";
+    guesses.style.display = "block";
+    number.style.display = "block";
+    text.style.display = "block";
+    submit.style.display = "block";
 
 });
+//Here we generate the number every time randomly
 var numbers = [0,1,2,3,4,5,6,7,8,9];
 function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
@@ -34,33 +37,31 @@ function shuffleArray(array) {
     }
     return array;
 }
-
-var hidden=shuffleArray(numbers).slice(0,4).join('');
+var hidden = shuffleArray(numbers).slice(0,4).join('');
 console.log(hidden);
 if(hidden[0] == 0){
-	hidden=numbers.slice(1,5).join('');
+	hidden = numbers.slice(1,5).join('');
 	console.log(hidden);
 }
    
   	// console.log(numbers);
-
+//This is the main function
 submit.addEventListener('click', play);
 function play() {
     var guess = document.getElementById('guess').value;
     var flag = true;
  	//console.log(guess)
 
-  	if(guess){
+  	if (guess) {
   		var arr=guess.split(""),
   			len=arr.length;
 			//console.log(arr)
 
-
-  		for(i=0;i<len;i++){
-  			var num =parseInt(arr[i])
-
-  			if(isNaN(num)){
-  				 flag = false;
+        //here we chek if the input value is != number
+  		for(i = 0;i < len; i++){
+  			var num = parseInt(arr[i])
+  			if (isNaN(num)) {
+  				flag = false;
                // alert('The number must contain only digits')
   			}
   		}//end for
@@ -77,8 +78,7 @@ function play() {
  		} else {
             clicks++;
             //console.log(clicks)
-            document.getElementById('guesses').textContent="Try: " + clicks;
-            
+            document.getElementById('guesses').textContent="Try: " + clicks;            
 			var bulls = 0;
     	    var cows = 0;
 			if (guess !== hidden) {
@@ -106,21 +106,20 @@ function play() {
                 document.getElementById('number').innerHTML="The number: " + guess;
 
     		} else if (guess === hidden) {
-      			message.innerHTML="Congratulations!";
+      			message.innerHTML = "Congratulations!";
                 message.setAttribute('id', 'gameover');
-                number.innerHTML="Your number: " + guess;
+                number.innerHTML = "Your number: " + guess;
       			submit.style.display = 'none';
                 btn.style.display = 'none';
                 var retry=document.createElement('a');
                 retry.setAttribute('href','index.html');
-                retry.setAttribute('id', 'gameover');
-                
-                retry.textContent='GameOver';
+                retry.setAttribute('id', 'gameover');                
+                retry.textContent = 'GameOver';
                 document.getElementById('play').appendChild(retry);
 
     		}//end guess hidden
     	}//if guess length === 4
-	}else{
+	} else {
 		alert('You must enter a number')
 	}//end if guess
     
